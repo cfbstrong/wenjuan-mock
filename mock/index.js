42d337394524eaab6fe12d0fd4d4f4488dbd9294
fengbin_chen <fengbin_chen@yunzhijia.com>
2025-08-06 22:59:52 +0800

    feature: add user interface

--- mock/index.js
+++ mock/index.js
@@ -2,6 +2,8 @@ const test = require("./test");
 
 const question = require("./question");
 
-const mockList = [...test, ...question];
+const user = require("./user");
+
+const mockList = [...test, ...question, ...user];
 
 module.exports = mockList;
