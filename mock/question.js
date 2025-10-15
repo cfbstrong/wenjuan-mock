de0aa72b57445b92ef26a1f8727caef27a0d2a7e
fengbin_chen <1563540102@qq.com>
2025-10-15 23:42:47 +0800

    feature: add isLocked attribute

--- mock/question.js
+++ mock/question.js
@@ -22,6 +22,7 @@ module.exports = [
               type: "questionTitle", //组件类型 <a/> <div/> <input/>
               title: "标题",
               isHidden: false,
+              isLocked: false,
               props: {
                 title: "一行标题",
                 level: 1,
@@ -34,6 +35,7 @@ module.exports = [
               type: "questionInput", //不能重复，前后端统一好
               title: "输入框1",
               isHidden: false,
+              isLocked: false,
               props: {
                 title: "你的电话", //目前是默认，一旦前端修改了属性，就会发送请求保存到服务器上，后面服务器返回的就是客户自定义的内容了
                 placeholder: "请输入电话...",
@@ -44,6 +46,7 @@ module.exports = [
               type: "questionInput", //不能重复，前后端统一好
               title: "输入框2",
               isHidden: false,
+              isLocked: false,
               props: {
                 title: "你的姓名", //目前是默认，一旦前端修改了属性，就会发送请求保存到服务器上，后面服务器返回的就是客户自定义的内容了
                 placeholder: "请输入姓名...",
